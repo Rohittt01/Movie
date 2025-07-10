631eb8da78a98ca5561e419a136d731255c200a7
Rohittt01 <124079221+Rohittt01@users.noreply.github.com>
2025-07-10 11:57:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Favorite from './pages/Favorite'
+import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
+
+function App() {
+
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App
